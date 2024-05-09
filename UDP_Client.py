a161fdaa1fc2ded770cea22d8c40ada95e0a78ee
Emir <62707230+thecloudrazor@users.noreply.github.com>
2024-05-09 12:49:24 +0000

    UDP
    bitmiş hali.

---
+++ UDP_Client.py
@@ -0,0 +1,23 @@
+from socket import *
+import time, json
+udpCliSock = socket(AF_INET, SOCK_DGRAM)  # opening a new UDP Socket
+
+def service_announcer():
+    print("Welcome to the UDP Client!"
+          "\n----------------------\n"
+          "To announce your presence to other users in the network, this client will be utilized.\n"
+          "\n----------------------\n")
+
+    username = input("Please enter your username to continue:")
+    username_as_json = {"username":username}
+
+    broadcast_addr = input("Now, enter the broadcast address of the network you're connected here: ")
+    print("Broadcasting your username across the network...\n"
+          "Unless you close this client, it will keep getting broadcast every 8 seconds.")
+    server_address = (broadcast_addr, 6000)
+    while True:
+        message = json.dumps(username_as_json).encode('utf-8')
+        udpCliSock.sendto(message, server_address)
+        time.sleep(8)
+
+service_announcer()
---
+++ UDP_Server.py
@@ -0,0 +1,74 @@
+from socket import *
+import os, threading, json
+import time as time
+from datetime import datetime, timedelta
+
+def load_user_data():
+      with open('contacts.json', 'w') as CreatingFile:
+            json.dump({"users": []}, CreatingFile)
+      with open('contacts.json', 'r') as OnlineUsers:
+            return json.load(OnlineUsers)
+
+def save_user_data(online_user_data):
+      with open('contacts.json', 'w') as ModifyingFile:
+            json.dump(online_user_data, ModifyingFile, indent=4, sort_keys=True)
+
+def listener(udp_socket, last_ping_time, online_user_data):
+      while True:
+            received_data, received_address = udp_socket.recvfrom(2048)
+            received_message = received_data.decode()
+            received_msg_as_json = json.loads(received_message)
+            new_user = received_msg_as_json['username']
+            timestamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            ip_address = received_address[0]
+            last_ping_time[new_user] = datetime.strptime(timestamp, "%d/%m/%Y %H:%M:%S")
+
+            if(new_user not in [user['username'] for user in online_user_data['users']]):
+                  new_entry = {"username": new_user, "IP Address": received_address[0], "Last Seen": timestamp, "Status": "Online"}
+                  online_user_data['users'].append(new_entry)
+                  save_user_data(online_user_data)
+                  print(f"New user found! | Username: {new_user} | IP: {ip_address} | Timestamp: {timestamp}")
+            else:
+                  for user in online_user_data['users']:
+                        if user['username'] == new_user:
+                              user['Status'] = "Online"
+                              user['Last Seen'] = timestamp
+                              save_user_data(online_user_data)
+                              break
+
+def user_status_checker(last_ping_time, online_user_data):
+      while True:
+            current_time = datetime.now()
+            for user in online_user_data['users']:
+                  if (current_time - last_ping_time.get(user['username'], current_time)).total_seconds() > 10:
+                        user['Status'] = 'Away'
+            save_user_data(online_user_data)
+            time.sleep(1)
+
+def peer_discovery():
+      print("Welcome to the UDP Server!\nThis is used for listing active users in the network.\n---------------------\n"
+            "Receiving status probes from other users...\n---------------------\n")
+
+      serverPort = 6000  # the UDP server's port. This will receive peer announcements
+      try:
+            udpServSock = socket(AF_INET, SOCK_DGRAM)
+            udpServSock.bind(('', serverPort))
+            # udpServSock.listen(15)
+            print("Socket successfully created.\n")
+      except socket.error as err:
+            print(f"An error occured: {err}")
+
+
+      last_ping_time = {}
+      online_user_data = load_user_data()
+
+      listener_thread = threading.Thread(target=listener, args=(udpServSock,last_ping_time, online_user_data))
+      status_checker_thread = threading.Thread(target=user_status_checker, args=(last_ping_time, online_user_data))
+
+      listener_thread.start()
+      status_checker_thread.start()
+
+      listener_thread.join()
+      status_checker_thread.join()
+
+peer_discovery()
